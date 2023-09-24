3d5281a43f171af1052f9fa97622c577bdc1a0de
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2023-09-24 14:44:02 +0300

    add create-nginx-virtual-hosts.py

---
+++ README.md
@@ -0,0 +1,42 @@
+# Nginx Virtual Hosts Creation Script
+
+This script is python version of [previous bash script](https://github.com/Noct2000/nginx-virtual-hosts-script).
+
+This python script automates the process of creating Nginx virtual hosts on an Ubuntu Server 22.04. It allows you to quickly set up multiple virtual hosts with minimal user input. 
+
+**NOTE**: Requires python3
+
+## Usage
+
+Before using this script, make sure you have Bash installed on your system. To create Nginx virtual hosts, follow these steps:
+
+1. **Download the Script**: Download the `create-nginx-virtual-hosts.sh` script to your server.
+
+    ```bash
+    wget https://github.com/Noct2000/nginx-virtual-hosts-script-py/raw/main/create-nginx-virtual-hosts.py
+    ```
+
+2. **Make the Script Executable**: If the script is not already executable, run the following command to make it executable:
+
+   ```bash
+    sudo chmod +x create-nginx-virtual-hosts.py
+   ```
+
+3. **Run the Script**: Execute the script with the desired virtual hostnames as command-line arguments. For example:
+
+   ```bash
+   sudo ./create-nginx-virtual-hosts.py myhost1.com myhost2.org myhost3.com
+   ```
+
+   Replace `myhost1.com`, `myhost2.org`, etc., with your actual virtual hostnames.
+
+4. **Follow the Instructions**: The generete you line that you must to add to your hosts file on your machine
+
+5. **Go to the web page of your host**: Open in your browser page by URL:
+
+    ```
+    http://myhost1.com
+    ```
+     
+    Replace `myhost1.com` with your actual virtual hostnames.
+    
---
+++ create-nginx-virtual-hosts.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import sys
+
+def install_package(package_name):
+    subprocess.run(['sudo', 'apt-get', 'update'])
+    subprocess.run(['sudo', 'apt-get', 'install', '-y', package_name])
+
+def create_virtual_host(hostname):
+    print(f"Creating: {hostname} virtual host")
+    
+    # Prepare host config
+    base_virtual_host_config = f"""
+    # Virtual Host configuration for {hostname}
+    #
+    # You can move that to a different file under sites-available/ and symlink that
+    # to sites-enabled/ to enable it.
+    #
+    server {{
+            listen 80;
+            listen [::]:80;
+
+            server_name {hostname};
+
+            root /var/www/{hostname}/html;
+            index index.html;
+
+            location / {{
+                    try_files $uri $uri/ =404;
+            }}
+    }}
+    """
+    
+    # Write config to /etc/nginx/sites-available/{hostname}
+    with open(f"/etc/nginx/sites-available/{hostname}", 'w') as config_file:
+        config_file.write(base_virtual_host_config)
+
+    print("Success")
+    
+    # Create base page for {hostname} virtual host
+    web_page = f"""
+    <html>
+        <body>
+            Hello from {hostname}
+        </body>
+    </html>
+    """
+    
+    # Create directory and write index.html
+    os.makedirs(f"/var/www/{hostname}/html", exist_ok=True)
+    with open(f"/var/www/{hostname}/html/index.html", 'w') as index_file:
+        index_file.write(web_page)
+
+    print("Success")
+
+def main():
+    # Get the arguments from the command-line except the filename
+    args = sys.argv[1:]
+    args_count = len(args)
+
+    if args_count == 0:
+        print("No command-line arguments provided.")
+        print("Please enter at least one virtual hostname for nginx")
+        print("Example:")
+        print("python create-nginx-virtual-hosts.py myhost1.com myhost2.org myhost3.com")
+    else:
+        # Check if OpenSSH Server is installed
+        if subprocess.run(['dpkg', '-l']).returncode != 0 or b"openssh-server" not in subprocess.check_output(['dpkg', '-l']):
+            print("OpenSSH Server is not installed. Installing...")
+            install_package("openssh-server")
+        else:
+            print("OpenSSH Server is already installed.")
+
+        # Check if Nginx is installed
+        if subprocess.run(['dpkg', '-l']).returncode != 0 or b"nginx" not in subprocess.check_output(['dpkg', '-l']):
+            print("Nginx is not installed. Installing...")
+            install_package("nginx")
+        else:
+            print("Nginx is already installed.")
+
+        # Stop nginx
+        subprocess.run(['sudo', 'systemctl', 'stop', 'nginx'])
+
+        # Loop through all virtual hostnames
+        for arg in args:
+            create_virtual_host(arg)
+
+        print("Fix error hash bucket memory")
+        subprocess.run(['sudo', 'sed', '-i', 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 64;/', '/etc/nginx/nginx.conf'])
+        print("Success")
+
+        print("Start nginx")
+        subprocess.run(['sudo', 'systemctl', 'start', 'nginx'])
+        
+        # Get IP address
+        ip_address = subprocess.check_output(['hostname', '-I']).decode().strip()
+        print("Success")
+
+        print("Please add the following line to your hosts file:")
+        print(f"{ip_address} {' '.join(args)}")
+        print("Usual Windows path: \"C:\\Windows\\System32\\drivers\\etc\\hosts\"")
+        print("Usual Linux path: \"/etc/hosts\"")
+
+if __name__ == "__main__":
+    main()
